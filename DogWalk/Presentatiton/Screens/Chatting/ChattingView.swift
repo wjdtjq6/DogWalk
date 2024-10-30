b09f21f4661da5ac49530856e5f9f7b8ed8d19b4
yoonwooiOS <yoonwoo4429@kakao.com>
2024-10-30 18:22:11 +0900

    Chore: 컨플릭 해결

--- DogWalk/Presentatiton/Screens/Chatting/ChattingView.swift
+++ DogWalk/Presentatiton/Screens/Chatting/ChattingView.swift
@@ -2,14 +2,28 @@
 //  ChattingView.swift
 //  DogWalk
 //
-//  Created by junehee on 10/29/24.
+//  Created by 김윤우 on 10/29/24.
 //
 
 import SwiftUI
 
 struct ChattingView: View {
+    @State private var searchText = "하이"
     var body: some View {
+<<<<<<< Updated upstream
         Text("ChattingView")
+=======
+        VStack {
+            HStack {
+                Text("MeongTalk")
+                       .font(.bagelfat28)
+                       .vTop()
+                Spacer()
+            }
+            .padding(.leading, 12)
+        }
+        .searchable(text: $searchText)
+>>>>>>> Stashed changes
     }
 }
 
